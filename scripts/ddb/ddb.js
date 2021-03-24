bef4e50230bf07505093e0f0553d9cc8c9663414
Drenam1 <bobthetomato29@gmail.com>
2021-03-24 20:33:03 +1300

    First attempt at getting DDB characters

--- scripts/ddb/ddb.js
+++ scripts/ddb/ddb.js
@@ -1,1 +1,19 @@
 console.log("Is this thing on?");
+var characterIds = ['43646479']
+
+for (var i=0;i<characterIds.length;i++){
+    async function get() {
+        let url = 'https://www.dndbeyond.com/profile/MrCandleguyDM/characters/'+characterIds[i]
+        let obj = await (await fetch(url)).json();
+        
+        console.log(obj);
+        return obj;
+    }
+    var tags;
+    (async () => {
+      tags = await get()
+      //console.log(tags)
+      document.getElementById("tags").innerHTML = JSON.stringify(tags);
+    })()
+    console.log(tags);
+}
