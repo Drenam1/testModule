0b33ab6bd30a41c01a3c952622fb579c97ddf221
Drenam1 <bobthetomato29@gmail.com>
2021-03-24 20:57:49 +1300

    Blank Module

--- scripts/ddb/ddb.js
+++ scripts/ddb/ddb.js
@@ -1,13 +1,1 @@
 console.log("Is this thing on?");
-var characterIds = ['43646479']
-
-let url = 'https://www.dndbeyond.com/profile/MrCandleguyDM/characters/'+characterIds[0]
-$.getJSON('http://time.jsontest.com', function(data) {
-        
-    var text = `Date: ${data.date}<br>
-                Time: ${data.time}<br>
-                Unix time: ${data.milliseconds_since_epoch}`
-                
-    
-    console.log(text)
-});
